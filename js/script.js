dd525a287206131e6fd499a47e0270ffe40831d1
MaksimShamin <invadebl@gmail.com>
2021-06-08 12:43:42 +0300

    1.01

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 	$('.slider').slick({
 		slidesToShow:1,
 		centerMode:true,
-		variableWidth:true,
-		/*appendArrows:$('.slider__item')*/
+		variableWidth:true
 	});
 });
 
